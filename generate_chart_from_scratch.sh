6524a8f3c74060a6e608af0264afe8d858869257
Joe Reddington <joe@joereddington.com>
2019-09-12 12:23:12 +0100

    getting this working again

--- generate_chart_from_scratch.sh
+++ generate_chart_from_scratch.sh
@@ -6,6 +6,6 @@ git rebase  --exec ../takesnapshot.sh --root --preserve-merges
 mv log.csv ../log.csv
 cd ../
 python plotPri.py  -f log.csv   -c -t 7 -o fear.png
-rm -Rf temp/
-git commit -a -m "update chart" 
-git push origin master
+#rm -Rf temp/
+#git commit -a -m "update chart" 
+#git push origin master
